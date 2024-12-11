fa97c1f55471b8511b56a23e46e5226d1b272891
Cameron Slash <calexslash@aol.com>
2024-12-11 00:23:48 -0500

    Got NextUI working

---
+++ people-web-app/src/app/people/page.tsx
@@ -0,0 +1,5 @@
+'use client';
+
+export default function People() {
+  return <h1>People</h1>;
+}
---
+++ people-web-app/tailwind.config.js
@@ -0,0 +1,20 @@
+import { nextui } from '@nextui-org/theme';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+      },
+    },
+  },
+  darkMode: 'class',
+  plugins: [nextui()],
+};
